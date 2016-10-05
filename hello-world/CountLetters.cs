044c3a589955846892e95f76615d9dd590c83901
Arunasr <Arunasr@VDITOT002>
2016-10-05 17:52:56 +0300

    Add most Common Letter Count Cycle

---
+++ hello-world/CountLetters.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace hello_world
+{
+    public class CountLetters
+    {
+     
+        public char Letter(string name)
+        {
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+            int max = 0;
+            foreach (char c in name)
+            {
+                int i;
+                dict.TryGetValue(c, out i);
+                i++;
+                if (i > max)
+                {
+                    max = i;
+                }
+                dict[c] = i;
+            }
+            foreach (KeyValuePair<char, int> chars in dict)
+            {if (chars.Value == max)
+                {
+                    return (chars.Key);
+                }
+             
+            }
+            //Console.ReadLine();
+            return char.MinValue;
+        }
+      
+            }
+}
--- hello-world/Program.cs
+++ hello-world/Program.cs
@@ -15,10 +15,12 @@ namespace hello_world
 
             var module = new hello_world_module();
             var module1 = new CapitalizeHello();
+            var module2 = new CountLetters();
 
 
             Console.WriteLine(module1.Lower(module.SayHello(args[0])));
-
+            Console.WriteLine("Most Commone Letter is: ");
+            Console.WriteLine(module2.Letter(args[0]));
             Console.ReadLine();
         }
     }
